800c2373228b4112e17f842c9f250c507487d244
zxsteven <breathesteve@gmail.com>
2015-12-24 21:53:59 +0000

    added controllers

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 var app = angular.module('myApp', []);
+
+app.controller('PlayerController', ['$scope', function($scope) {
+  $scope.playing = false;
+  $scope.audio = document.createElement('audio');
+  $scope.audio.src = '/media/npr.mp4';
+  $scope.play = function() {
+    $scope.audio.play();
+    $scope.playing = true;
+  };
+  $scope.stop = function() {
+    $scope.audio.pause();
+    $scope.playing = false;
+  };
+  $scope.audio.addEventListener('ended', function() {
+    $scope.$apply(function() {
+      $scope.stop()
+    });
+  });
+}]);
+
+app.controller('RelatedController', ['$scope', function($scope){}]);
